8f4d18391fe1ce312de9af9ebc4cd534e9f5851a
Tobi Adeleye <adelleye@yahoo.com>
2020-02-05 14:05:44 -0500

    Certificates View done

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,31 +10,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
             
-            BackCardView()
-                .background(Color("card4"))
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                .offset(x: 0, y: -40)
-                .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
-                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0.0, z: 0.0))
-                .blendMode(.hardLight)
-            
-            BackCardView()
-                .background(Color("card3"))
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                .offset(x: 0, y: -20)
-                .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
-                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0.0, z: 0.0))
-                .blendMode(.hardLight)
-            
-            CardView()
-                .blendMode(.hardLight)
-        }
+      
+            ZStack {
+                TitleView()
+                
+             
+                
+                BackCardView()
+                    .background(Color("card4"))
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .offset(x: 0, y: -40)
+                    .scaleEffect(0.9)
+                    .rotationEffect(.degrees(10))
+                    .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0.0, z: 0.0))
+                    .blendMode(.hardLight)
+                
+                BackCardView()
+                    .background(Color("card3"))
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .offset(x: 0, y: -20)
+                    .scaleEffect(0.95)
+                    .rotationEffect(.degrees(5))
+                    .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0.0, z: 0.0))
+                    .blendMode(.hardLight)
+                
+                CardView()
+                    .blendMode(.hardLight)
+                
+                VStack (spacing: 20) {
+                    
+                    Rectangle()
+                        .frame(width: 40, height: 5, alignment: .center)
+                        .cornerRadius(3)
+                        .opacity(0.1)
+                    
+                    Text("This certificate is proof that Tobi Adeleye has achieved the UI Design course with approval from a Design+Code instructor.")
+                        .multilineTextAlignment(.center)
+                        .font(.subheadline)
+                        .lineSpacing(4)
+                        Spacer()
+
+                        
+                   
+                    
+                }
+                .padding(.top, 10)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .offset(x: 0, y: 500)
+            }
+        
     }
 }
 
@@ -54,7 +85,7 @@ struct CardView: View {
                     
                     VStack (alignment: .leading) {
                         Text("UI Design")
-                            .font(.largeTitle)
+                            .font(.title)
                             .fontWeight(.bold)
                             .foregroundColor(.white)
                         
@@ -93,5 +124,21 @@ struct BackCardView: View {
         .frame(width: 340, height: 220)
         
        
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
     }
 }
