f8a822b446e2d375e106d35704f6e7b5d0c63384
Tobi Adeleye <adelleye@yahoo.com>
2020-02-07 13:52:44 -0500

    Added Animations to HomeView

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -12,27 +12,57 @@ struct HomeView: View {
     @State var showMenu = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Watching")
-                    .font(.title)
-                    .fontWeight(.bold)
+        ZStack {
+            
+            Color(#colorLiteral(red: 0.8549019608, green: 0.8745098039, blue: 0.9176470588, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+                
+            
+            VStack {
+                 
+                HStack {
+                    Text("Watching")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button(action: {self.showMenu.toggle()} ) {
+                           Image("Avatar")
+                            .renderingMode(.original) // removes blend
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                        .clipShape(Circle())
+                           }
+                   
+                }
+                .padding(.horizontal)
                 Spacer()
-                Button(action: {self.showMenu.toggle()} ) {
-                       Image("Avatar")
-                        .renderingMode(.original) // removes blend
-                        .resizable()
-                        .frame(width: 36, height: 36)
-                    .clipShape(Circle())
-                       }
-               
+                
+              
             }
-            .padding(.horizontal)
-            Spacer()
+            .padding(.top, 44)
+            .background(Color.white)
+              
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .circular))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .offset(y: showMenu ? -500 : 0)
+            .rotation3DEffect(Angle(degrees: showMenu ? -5 : 0), axis: (x: 10.0 , y: 0, z: 0))
+            
+            .scaleEffect( showMenu ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            
             
             MenuView()
+            
+                .background(Color.black.opacity(0.001))
+               
                 .offset( y: showMenu ? 0 : 1000)
                 .animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
+                .onTapGesture {
+                     self.showMenu.toggle()
+            }
+            
+           
         }
         
        
